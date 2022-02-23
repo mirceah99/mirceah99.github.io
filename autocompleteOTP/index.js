4a76722fab44f0408bb56daf7773efe039d45d87
mirceah99 <81265688+mirceah99@users.noreply.github.com>
2022-02-23 22:41:43 +0200

    Update index.js

--- autocompleteOTP/index.js
+++ autocompleteOTP/index.js
@@ -1,18 +1,23 @@
-try{
-    if(window.OTPCredential){
-const p = document.getElementById("your-code");
+// try{
+//     if(window.OTPCredential){
+// const p = document.getElementById("your-code");
+// navigator.credentials.get({
+//     otp: {transport:['sms']},
+//     signal: (new AbortController()).signal
+// }).then( otp => {
+//     alert (document.getElementById("your-code"));
+//     alert('then');
+//     alert (document.getElementById("your-code"));
+//     alert('then2');
+//     alert(JSON.stringify(otp));
+//     p.value = otp.code;
+// }).catch(e =>{
+//     p.value = e;
+// })
+//     } else {alert("not possible");}
+// } catch(e) { alert(JSON.stringify(e));}
 navigator.credentials.get({
-    otp: {transport:['sms']},
-    signal: (new AbortController()).signal
-}).then( otp => {
-    alert (document.getElementById("your-code"));
-    alert('then');
-    alert (document.getElementById("your-code"));
-    alert('then2');
-    alert(JSON.stringify(otp));
-    p.value = otp.code;
-}).catch(e =>{
-    p.value = e;
-})
-    } else {alert("not possible");}
-} catch(e) { alert(JSON.stringify(e));}
+    otp: {
+      transport: ["sms"]
+    }
+  }).then(r => alert(JSON.stringify(r))).catch(e => alert(JSON.stringify(e)));
