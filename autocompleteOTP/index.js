ff8aa72e89b0d23398ec7a6ec4a2b5ef7527408c
mirceah99 <81265688+mirceah99@users.noreply.github.com>
2022-02-23 21:58:24 +0200

    2 commit

--- autocompleteOTP/index.js
+++ autocompleteOTP/index.js
@@ -1,9 +1,10 @@
 const p = document.getElementById("your-code");
 navigator.credentials.get({
-    otp: {transport:['sms']}
+    otp: {transport:['sms']},
+    signal: (new AbortController()).signal
 }).then( otp => {
+    alert('then');
     p.value = otp.code;
-
 }).catch(e =>{
     p.value = e;
 })
